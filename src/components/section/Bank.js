3efe79971dba5f473462e9536c7cc50df1327828
hyoni-ch <asdfgh9933@daum.net>
2023-11-28 16:08:19 +0900

    account 부분 수정

--- src/components/section/Bank.js
+++ src/components/section/Bank.js
@@ -34,7 +34,7 @@ export default function Bank() {
         <h2 className="title pointColor">Account</h2>
       </Fade>
 
-      <section className="spacerB50 flex">
+      <section className="spacerB50 flex flex-col">
         <Fade bottom delay={500}>
           <h3 className="padding-3">마음 전하실 곳</h3>
 
--- src/components/section/Intro.js
+++ src/components/section/Intro.js
@@ -11,7 +11,7 @@ export default function Intro() {
           {/* <img src={date_image} alt="날짜 이미지" width="65px" /> */}
           <div className="wedding-date-num wedding-date-num-1">03</div>
           <div className="wedding-date-line"></div>
-          <div className="wedding-date-num wedding-date-num-2">17</div>
+          <div className="wedding-date-num wedding-date-num-2">24</div>
         </div>
         <div className="wedding-name">조하나</div>
       </section>
