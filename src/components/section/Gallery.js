c9f99741810bbbb4b5a00b153c650b1802b18161
hyoni-ch <asdfgh9933@daum.net>
2024-01-07 04:29:35 +0900

    갤러리 push onMouseOver 추가

--- src/components/section/Gallery.js
+++ src/components/section/Gallery.js
@@ -147,6 +147,7 @@ export default function Gallery() {
                 className="w-full"
                 onClick={() => onClickImage(index)}
                 onMouseDown={onMouseDownImage}
+                onMouseOver={onMouseDownImage}
               />
             ))}
           </div>
