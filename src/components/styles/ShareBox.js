5233a132ede8bd64c3383185c74cc415f8247be5
hyoni-ch <asdfgh9933@daum.net>
2024-01-07 00:03:29 +0900

    공유하기 버튼 크기 조정

--- src/components/styles/ShareBox.js
+++ src/components/styles/ShareBox.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 const ShareBox = styled.div`
   .btn-share {
     padding: 0.8rem 1rem 0.8rem 1rem;
-    max-width: 10rem;
     font-size: 1rem;
+    width: 10rem;
+    @media screen and (max-width: 350px) {
+      width: 95%;
+    }
   }
 `;
 
